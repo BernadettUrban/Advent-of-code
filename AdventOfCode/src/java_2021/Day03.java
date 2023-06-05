62eebf638d34ca74dd00f24d7abf1ac07b2c67ad
Bernadett Urban <bernadett.urban@outlook.com>
2023-06-05 14:57:10 +0200

    refactor day 3

--- AdventOfCode/src/java_2021/Day03.java
+++ AdventOfCode/src/java_2021/Day03.java
@@ -1,13 +1,11 @@
 package java_2021;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day03 {
-    public static void main(String[] args) {
-        FileReaderClass fileReaderClass = new FileReaderClass();
-        List<String> list =  fileReaderClass.readFileInList("AdventOfCode\\src\\txt_2021\\03.txt");
-        System.out.println(list);
+
+    public static char[][] getTransposedArray(List<String> list){
         int length = list.size();
         int lengthOfWord = list.get(0).length();
         char[][]  array = new char[length][lengthOfWord];
@@ -23,7 +21,9 @@ public class Day03 {
                 transposedArray[i][j]= array[j][i];
             }
         }
-
+        return transposedArray;
+    }
+    public static void partOne( char[][] transposedArray){
         StringBuilder gamma = new StringBuilder();
         StringBuilder epsilon = new StringBuilder();
 
@@ -53,5 +53,44 @@ public class Day03 {
         int decimalGamma=Integer.parseInt(gamma.toString(),2);
         int decimalEpsilon=Integer.parseInt(epsilon.toString(),2);
         System.out.println(decimalGamma*decimalEpsilon);
+    }
+    public static void main(String[] args) {
+        FileReaderClass fileReaderClass = new FileReaderClass();
+        List<String> list =  fileReaderClass.readFileInList("AdventOfCode\\src\\txt_2021\\03ex.txt");
+        char[][] transposedArray= getTransposedArray(list);
+        //partOne(transposedArray);
+        System.out.println(list);
+        List<String>binaryNums = new ArrayList<>();
+        StringBuilder gamma = new StringBuilder();
+        StringBuilder epsilon = new StringBuilder();
+
+        for (int i = 0; i < transposedArray.length; i++) {
+            int countZero = 0;
+            int countOne = 0;
+            for (int j = 0; j < transposedArray[i].length; j++) {
+                if(transposedArray[i][j]=='0'){
+                    countZero++;
+                }else{
+                    countOne++;
+                }
+            }
+            if(countZero>countOne){
+                for (int j = 0;j< list.size();j++){
+                    if(list.get(j).charAt(0)=='0'){
+                        binaryNums.add(list.get(j));
+                    }
+                }
+
+            }else if(countOne>countZero){
+                for (int j = 0;j< list.size();j++){
+                    if(list.get(j).charAt(0)=='1'){
+                        binaryNums.add(list.get(j));
+                    }
+                }
+            }
+            System.out.println();
+
+        }
+        System.out.println(binaryNums);
     }
 }
